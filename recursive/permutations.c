5bfbc14d5472f86f211862409dc6372722346894
Cristian Mocanu <cristian.mocanu@luxon-tech.com>
2026-09-12 14:45:16 +0300

    format

--- recursive/permutations.c
+++ recursive/permutations.c
@@ -6,125 +6,127 @@
 // print the array
 void output_array(long n, unsigned *a)
 {
-	long i;
-	for (i = 0; i < n; i++)
-	{
-		printf("%d ", a[i]);
-	}
-	printf("\n");
+  long i;
+  for (i = 0; i < n; i++)
+    {
+      printf("%d ", a[i]);
+    }
+  printf("\n");
 }
 
 // print all the arrays and free the memory
 void output(long m, int n, unsigned **p)
 {
-	long i;
-	for (i = 0; i < m; i++)
-	{
-		output_array(n, p[i]);
-		free(p[i]);
-	}
-	free(p);
+  long i;
+  for (i = 0; i < m; i++)
+    {
+      output_array(n, p[i]);
+      free(p[i]);
+    }
+  free(p);
 }
 
 long factorial(int n)
 {
-	int i;
-	long f = 1;
-	for (i = 2; i <= n; i++)
-	{
-		f *= i;
-	}
-	return f;
+  int i;
+  long f = 1;
+  for (i = 2; i <= n; i++)
+    {
+      f *= i;
+    }
+  return f;
 }
 
 // insert e at every position in a
 unsigned** combine(long n, unsigned e, unsigned *a)
 {
-	long i;
-	unsigned **b;
-	b = malloc(n * sizeof(int*));
-	for (i = 0; i < n; i++)
-	{
-		b[i] = malloc(n * sizeof(int));
-		b[i][i] = e;
-		memcpy(b[i], a, i * sizeof(int));
-		memcpy(&b[i][i + 1], &a[i], (n - i - 1) * sizeof(int));
-	}
-	return b;
+  long i;
+  unsigned **b;
+  b = malloc(n * sizeof(int*));
+  for (i = 0; i < n; i++)
+    {
+      b[i] = malloc(n * sizeof(int));
+      b[i][i] = e;
+      memcpy(b[i], a, i * sizeof(int));
+      memcpy(&b[i][i + 1], &a[i], (n - i - 1) * sizeof(int));
+    }
+  return b;
 }
 
 unsigned** permutations(long m, long n, unsigned *a)
 {
-	long i, j, k, t;
-	unsigned **c, **d, **e;
-	if (n <= 0)
+  long i, j, k, t;
+  unsigned **c, **d, **e;
+  if (n <= 0)
+    {
+      return NULL;
+    }
+  else if (n == 1)
+    {
+      e = malloc(sizeof(int*));
+      e[0] = a;
+      return e;
+    }
+  else
+    {
+      e = malloc(m * sizeof(int*));
+      k = 0;
+      t = m / n;
+      c = permutations(t, n - 1, a);
+      for (i = 0; i < t; i++)
 	{
-		return NULL;
+	  d = combine(n, a[n - 1], c[i]);
+	  for (j = 0; j < n; j++)
+	    {
+	      e[k++] = d[j];
+	    }
 	}
-	else if (n == 1)
-	{
-		e = malloc(sizeof(int*));
-		e[0] = a;
-		return e;
-	}
-	else
-	{
-		e = malloc(m * sizeof(int*));
-		k = 0;
-		t = m / n;
-		c = permutations(t, n - 1, a);
-		for (i = 0; i < t; i++)
-		{
-			d = combine(n, a[n - 1], c[i]);
-			for (j = 0; j < n; j++)
-			{
-				e[k++] = d[j];
-			}
-		}
-		free(c);
-		return e;
-	}
+      free(c);
+      return e;
+    }
 }
 
 void main(int argc, char **argv)
 {
-	int i, n, opt, c = 2;
-	long m;
-	unsigned *a;
-	while ((opt = getopt(argc, argv, "fp")) != -1) {
-		switch (opt) {
-			case 'f':
-				c = 1; // factorial
-				break;
-			case 'p':
-				c = 2; // permutations
-				break;
-			default:
-				fprintf(stderr, "Usage: %s [-f][-p] n\n", argv[0]);
-				exit(EXIT_FAILURE);
-		}
-	}
-	if (optind >= argc) {
-		fprintf(stderr, "Missing argument n\n");
-		exit(EXIT_FAILURE);
-	}
-	n = atoi(argv[optind]);
-	if (n > 0)
+  int i, n, opt, c = 2;
+  long m;
+  unsigned *a;
+  while ((opt = getopt(argc, argv, "fp")) != -1)
+    {
+      switch (opt) {
+      case 'f':
+	c = 1; // factorial
+	break;
+      case 'p':
+	c = 2; // permutations
+	break;
+      default:
+	fprintf(stderr, "Usage: %s [-f][-p] n\n", argv[0]);
+	exit(EXIT_FAILURE);
+      }
+    }
+  if (optind >= argc)
+    {
+      fprintf(stderr, "Missing argument n\n");
+      exit(EXIT_FAILURE);
+    }
+  n = atoi(argv[optind]);
+  if (n > 0)
+    {
+      a = malloc(n * sizeof(int));
+      for (i = 0; i < n; i++)
 	{
-		a = malloc(n * sizeof(int));
-		for (i = 0; i < n; i++)
-		{
-			a[i] = i + 1;
-		}
-		m = factorial(n);
-		if (c == 1)
-		{
-			printf("%d\n", m);
-		} else {
-			output(m, n, permutations(m, n, a));
-			if (n > 1) {
-				free(a);
-			}
-		}
+	  a[i] = i + 1;
 	}
+      m = factorial(n);
+      if (c == 1)
+	{
+	  printf("%d\n", m);
+	} else {
+	output(m, n, permutations(m, n, a));
+	if (n > 1) {
+	  free(a);
+	}
+      }
+    }
 }
